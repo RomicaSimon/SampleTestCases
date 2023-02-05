30d1f7cc7845f4240f3b084cfc9c90d53d79a3c7
Romica Simon <romica.simon000@gmail.com>
2023-02-05 20:25:32 +0200

    Added comments for the workflow

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -5,6 +5,17 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class App
 {
+    //   Using Browser commands
+    // 1. Open the Chrome browser
+    // 2. Navigate to the Url: https://www.emag.ro/
+    // 3. Maximize the window
+    // 4. Verify the current Url of the page
+    // 5. Verify the title of the page
+    // 6. Print the length of the title of the page
+    // 7. Print the page source of the page
+    // 8. Click on new tab
+    // 9. Close the browser
+    // 10. Quit the browser
     public static void main( String[] args ) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver","C://Users//ACER//Downloads//chromedriver.exe");
         WebDriver driver = new ChromeDriver();
