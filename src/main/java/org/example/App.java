40180cf76a96d9b13e6668d93bf239be239d2b23
Romica Simon <romica.simon000@gmail.com>
2023-02-05 20:10:29 +0200

    Launching the Chrome browser with ChromeDriver

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,9 +1,18 @@
 package org.example;
 
-public class App 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver","C://Users//ACER//Downloads//chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        Thread.sleep(2000);
+        driver.quit();
+
+
+
+
     }
 }
