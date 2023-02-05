ea1ca26e06c400858d8d7ef4ce54ffe8894326f7
Romica Simon <romica.simon000@gmail.com>
2023-02-05 20:53:11 +0200

    Test Case completed successfully

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -19,7 +20,31 @@ public class App
     public static void main( String[] args ) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver","C://Users//ACER//Downloads//chromedriver.exe");
         WebDriver driver = new ChromeDriver();
+        driver.get("https://www.emag.ro/");
+        driver.manage().window().maximize();
+        String pageUrl = driver.getCurrentUrl();
+        System.out.println("The current Url of the page is " + pageUrl);
+        if (pageUrl.equals("https://www.emag.ro/")) {
+            System.out.println("The page Url is correct");
+        }
+        else{
+            System.out.println("The page Url is incorrect");
+        }
+        String pageTitle = driver.getTitle();
+        System.out.println("The title of the page is  " + pageTitle);
+        if (pageTitle.equals("eMAG.ro - Căutarea nu se oprește niciodată")) {
+            System.out.println("The title of the page is correct");
+        }
+        else {
+            System.out.println("The title of the page is incorrect");
+        }
+        int titleLength = driver.getTitle().length();
+        System.out.println("The length of the title is " + titleLength);
+        //String pageSource = driver.getPageSource();
+        //System.out.println("The page source is " + pageSource);
+        driver.findElement(By.xpath("//a[@title='eMAG Help']")).click();
         Thread.sleep(2000);
+        //driver.close();
         driver.quit();
 
 
