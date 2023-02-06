2db6c5801d55165e5bc26bea4e9af1c01e952c5b
Romica Simon <romica.simon000@gmail.com>
2023-02-06 16:09:13 +0200

    Navigate from bigfish.ro to altex.ro

--- src/main/java/org/example/AppNavigation.java
+++ src/main/java/org/example/AppNavigation.java
@@ -17,9 +17,15 @@ public class AppNavigation {
      // 9. Close the browser
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver","C://Users//ACER//Downloads//chromedriver.exe");
         WebDriver driver = new ChromeDriver();
+        driver.get("https://www.bigfish.ro/");
+        driver.manage().window().maximize();
+        Thread.sleep(2000);
+        driver.navigate().to("https://altex.ro/");
+
+        Thread.sleep(2000);
         driver.quit();
     }
 }
